c5a3a34895378d010ba154ce8bea55d25fecb139
OpMAY <65110458+OpMAY@users.noreply.github.com>
2021-02-09 16:44:55 +0900

    브랜치 합병 충돌 수정

--- app/src/main/java/com/application/coronalive/main/MainActivity.kt
+++ app/src/main/java/com/application/coronalive/main/MainActivity.kt
@@ -1,21 +1,38 @@
 package com.application.coronalive.main
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.application.coronalive.R
 import com.application.coronalive.databinding.ActivityMainBinding
+import com.application.coronalive.fragments.DomesticFragment
+import com.application.coronalive.fragments.WorldFragment
+import com.application.coronalive.fragments.adapters.ViewPagerAdapter
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.home.*
+import kotlinx.android.synthetic.main.navigationdrawer.*
+import kotlinx.android.synthetic.main.toolbar.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main) -> Binding 작업으로 필요없는 구문
         val binding : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        setUpTabs()
+
+        menu.setOnClickListener { layout_drawer.openDrawer(GravityCompat.START) }
+        navigationView.setNavigationItemSelectedListener(this)
         subscribeUI(binding)
     }
 
@@ -36,5 +53,31 @@ class MainActivity : AppCompatActivity() {
         }
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings -> Toast.makeText(this, "설정", Toast.LENGTH_SHORT).show()
+            R.id.region -> Toast.makeText(this, "전체 지역별", Toast.LENGTH_SHORT).show()
+            R.id.faq -> Toast.makeText(this, "FAQ 및 제보하기", Toast.LENGTH_SHORT).show()
+            R.id.share -> Toast.makeText(this, "SNS 공유", Toast.LENGTH_SHORT).show()
+            R.id.message -> Toast.makeText(this, "재난문자", Toast.LENGTH_SHORT).show()
+            R.id.guidelines -> Toast.makeText(this, "거리두기 지침", Toast.LENGTH_SHORT).show()
+        }
+        layout_drawer.closeDrawers()
+        return false
+    }
+
+    override fun onBackPressed() {
+        if (layout_drawer.isDrawerOpen(GravityCompat.START)) layout_drawer.closeDrawers()
+        else super.onBackPressed()
+    }
+
+    private fun setUpTabs() {
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(DomesticFragment(), "국내")
+        adapter.addFragment(WorldFragment(), "세계")
+        viewPager.adapter = adapter
+        tabs.setupWithViewPager(viewPager)
+
     }
 }
